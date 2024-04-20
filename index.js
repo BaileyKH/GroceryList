f03ee01684d42374ee648e03dd0383a6fe90fa2c
BaileyKH <106282001+BaileyKH@users.noreply.github.com>
2024-04-20 12:01:13 -0700

    database fix and function refactors

--- index.js
+++ index.js
@@ -1,30 +1,45 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
-    databaseUrl: "https://playground-2231c-default-rtdb.firebaseio.com/"
+    databaseURL: "https://realtime-database-37e45-default-rtdb.firebaseio.com/"
 }
 
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
 const shoppingListInDB = ref(database, "shoppingList")
 
-const inputField = document.getElementById("input-field")
-const addBtn = document.getElementById("add-btn")
-const shoppingList = document.getElementById("shopping-list")
+const inputFieldEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-button")
+const shoppingListEl = document.getElementById("shopping-list")
 
-addBtn.addEventListener("click", () => {
-    let inputValue = inputField.value
+addButtonEl.addEventListener("click", function() {
+    let inputValue = inputFieldEl.value
+    
     push(shoppingListInDB, inputValue)
     
-    clearInput()
-    addListItems(inputValue)
+    clearInputFieldEl()
+
 })
 
-function clearInput() {
-    inputField.value = ""
+onValue(shoppingListInDB, function(snapshot) {
+    let itemsArray = Object.values(snapshot.val())
+
+    clearShoppingListEl();
+
+    for (let i = 0; i < itemsArray.length; i++) {
+        appendItemToShoppingListEl(itemsArray[i])
+    }
+})
+
+function clearShoppingListEl() {
+    shoppingListEl.innerHTML = ""
 }
 
-function addListItems(item) {
-    shoppingList.innerHTML += `<li>${item}</li>`
+function clearInputFieldEl() {
+    inputFieldEl.value = ""
+}
+
+function appendItemToShoppingListEl(itemValue) {
+    shoppingListEl.innerHTML += `<li class="list-items">${itemValue}</li>`
 }
